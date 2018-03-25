d39fac7a2e81f77bf810036be9ac73e9dde0f693
Victoria Kean <VictoriaKean@victorias-mbp.attlocal.net>
2018-03-25 16:00:21 -0400

    all done

--- code.rb
+++ code.rb
@@ -42,8 +42,42 @@ winning_canditate = vote_results[3].index(max_vote)
 puts "#{candidates[winning_canditate]} is the winner of Precinct 4."
 
 # How many people voted for Mary Sue?
-vote_results
 # How many people voted for Billy Joe?
 # How many people voted for Sally Jane?
+mary_sue_sum = 0
+billy_joe_sum = 0
+sally_jane_sum = 0
+vote_results.each do |precinct|
+  mary_sue_sum += precinct[0]
+  billy_joe_sum += precinct[2]
+  sally_jane_sum += precinct[1]
+end
+puts "Mary Sue had #{mary_sue_sum} people vote for her."
+puts "Billy Joe had #{billy_joe_sum} people vote for him."
+puts "Sally Jane had #{sally_jane_sum} people vote for her."
+
 # How many people voted in total?
+total_votes = 0
+vote_results.each do |precinct|
+  precinct.each do |candidate_votes|
+    total_votes += candidate_votes
+  end
+end
+puts "#{total_votes} people voted in total."
+
 # Who had the most votes?
+mary_sue_sum = 0
+billy_joe_sum = 0
+sally_jane_sum = 0
+vote_results.each do |precinct|
+  mary_sue_sum += precinct[0]
+  billy_joe_sum += precinct[2]
+  sally_jane_sum += precinct[1]
+end
+vote_tally = {
+  "Mary Sue" => mary_sue_sum,
+  "Billy Joe" => billy_joe_sum,
+  "Sally Jane" => sally_jane_sum
+}
+winner = vote_tally.sort_by {|k, v| v}.reverse[0]
+puts "#{winner[0]} had the most votes at #{vote_tally.values.max}"
